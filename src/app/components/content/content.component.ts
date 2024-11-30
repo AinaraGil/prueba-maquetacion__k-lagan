c2cb21c0e76a6667c6817e549a40c7cafa2279e1
AinaraGil <ainaragilcm@gmail.com>
2024-11-30 17:23:40 +0100

    new(prices): creado nuevo componente prices y maquetado, mejorados estilos de contente components

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { DestinationsComponent } from './sections/destinations/destinations.component';
 import { PlacesComponent } from './sections/places/places.component';
 import { OffersComponent } from './sections/offers/offers.component';
+import { PricesComponent } from './sections/prices/prices.component';
 
 @Component({
     selector: 'app-content',
-    imports: [DestinationsComponent, PlacesComponent, OffersComponent],
+    imports: [DestinationsComponent, PlacesComponent, OffersComponent, PricesComponent],
     templateUrl: './content.component.html',
     styleUrl: './content.component.scss',
 })
