102ca2343d33b74c80ebfbe0aaaf7c3a12c9472b
AinaraGil <ainaragilcm@gmail.com>
2024-11-30 14:38:09 +0100

    new(components): creados componentes offers y card, modificados estilos de places, content y destinations

--- src/app/components/content/content.component.ts
+++ src/app/components/content/content.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DestinationsComponent } from './sections/destinations/destinations.component';
 import { PlacesComponent } from './sections/places/places.component';
+import { OffersComponent } from './sections/offers/offers.component';
 
 @Component({
     selector: 'app-content',
-    imports: [DestinationsComponent, PlacesComponent],
+    imports: [DestinationsComponent, PlacesComponent, OffersComponent],
     templateUrl: './content.component.html',
     styleUrl: './content.component.scss',
 })
